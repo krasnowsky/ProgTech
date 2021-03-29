0cfa3ece700ca965bc3e42153625ba9d51e137c2
Wojciech <230453@edu.p.lodz.pl>
2021-03-29 17:52:39 +0200

    Simple test project with unit test

---
+++ test_library/test_library/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace test_library
+{
+    public class Calculator
+    {
+        private int sum;
+        private int initialValue;
+
+        public Calculator(int value) {
+            initialValue = value;
+        }
+
+        public void add(int a)
+        {
+            sum = a + initialValue;
+        }
+
+        public int getSum()
+        {
+            return sum;
+        }
+    }
+}
---
+++ test_library/test_libraryTests/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using test_library;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test_library.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        private int expectedSum = 5;
+
+        [TestMethod()]
+        public void CalculatorTest()
+        {
+            Calculator calculator = new Calculator(3);
+            calculator.add(2);
+            Assert.AreEqual(expectedSum, calculator.getSum());
+        }
+    }
+}
