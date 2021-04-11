ee2093070108a61c6c07492ecc0f2c9f3770422f
Wojciech <230453@edu.p.lodz.pl>
2021-04-11 13:56:20 +0200

    SmallShopData class added

---
+++ task_1/small_shop/small_shop/Data/SmallShopData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace small_shop.Data
+{
+    public class SmallShopData
+    {
+        private List<Customer> customers = new List<Customer>();
+        private List<Cart> carts = new List<Cart>();
+        private List<Product> products;
+        private List<State> states;
+        private List<Event> events;
+        public SmallShopData () { }
+
+        public void Add_Customer(Customer new_customer, Cart new_cart)
+        {
+            this.customers.Add(new_customer);
+            this.carts.Add(new_cart);
+        }
+
+        public int Get_Number_Of_Customer()
+        {
+            return this.customers.Count;
+        }
+
+
+    }
+}
---
+++ task_1/small_shop/small_shopTests/Data/SmallShopDataTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using small_shop.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace small_shop.Data.Tests
+{
+    [TestClass()]
+    public class SmallShopDataTests
+    {
+        [TestMethod()]
+        public void AddCustomerTest()
+        {
+            SmallShopData small_shop = new SmallShopData();
+            Customer John = new Customer("John", "Smith", "1");
+            Cart Johns_Cart = new Cart();
+
+            small_shop.Add_Customer(John, Johns_Cart);
+
+            Assert.AreEqual(1, small_shop.Get_Number_Of_Customer());
+        }
+    }
+}
