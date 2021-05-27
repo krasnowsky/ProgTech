4e9d69a9b39cb50a47c44d31f434ac365315374c
Maria Golińska <230448@edu.p.lodz.pl>
2021-05-27 22:30:19 +0200

    Another classes added

---
+++ task_2/SmallShopLibrary/SmallShopLibrary/DataFiles/Event.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SmallShopData.DataFiles
+{
+    public class Event
+    {
+        public Event()
+        {
+        }
+    }
+}
---
+++ task_2/SmallShopLibrary/SmallShopLibrary/DataFiles/State.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SmallShopData.DataFiles
+{
+    public class State
+    {
+        public State()
+        {
+        }
+    }
+}
